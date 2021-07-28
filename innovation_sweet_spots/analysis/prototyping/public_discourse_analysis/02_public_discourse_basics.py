397a90ada92b5c4cb176670e7a9a7f9bdf3ff43c
djyldyz <djyldyz@users.noreply.github.com>
2021-07-28 16:08:06 +0100

    Add sections to show number of articles per year and top 10 most common categories.

--- innovation_sweet_spots/analysis/prototyping/public_discourse_analysis/02_public_discourse_basics.py
+++ innovation_sweet_spots/analysis/prototyping/public_discourse_analysis/02_public_discourse_basics.py
@@ -29,6 +29,9 @@
 # ## 1. Import dependencies
 
 # %%
+# cd '/Users/jdjumalieva/Documents/Analysis/innovation_sweet_spots/'
+
+# %%
 from innovation_sweet_spots.getters import guardian
 from innovation_sweet_spots.analysis import analysis_utils as iss
 from innovation_sweet_spots.analysis import discourse_utils as disc
@@ -44,6 +47,7 @@ import numpy as np
 import collections
 import pickle
 import os
+from itertools import groupby
 # %%
 TAGS = ['p', 'h2']
 # OUTPUTS_CACHE = OUTPUT_DATA_PATH / ".cache"
@@ -52,42 +56,57 @@ DISC_OUTPUTS_DIR.mkdir(parents=True, exist_ok=True)
 # %% [markdown]
 # ## 2. Fetching relevant Guardian articles
 
+# %%
 search_terms = ['heat pumps']
 search_term = search_terms[0]
 
 articles = guardian.search_content(search_term)
 
-# %% [markdown]
-# ## 2.1 Review most common article categories
+# %%
+# group returned articles by year
+sorted_articles = sorted(articles, key = lambda x: x['webPublicationDate'][:4])
 
-article_categories = [article['sectionName'] for article in articles]
-category_count = sorted(collections.Counter(article_categories).items(), 
-                        key = lambda x: x[1], 
-                        reverse=True)
+articles_by_year = collections.defaultdict(list)
+for k,v in groupby(sorted_articles,key=lambda x:x['webPublicationDate'][:4]):
+    articles_by_year[k] = list(v)
+
+# %% [markdown]
+# ### 2.1 Mentions in the news
+
+# %%
+disc.show_num_articles(disc.get_num_articles(articles_by_year))
+
+# %% [markdown]
+# ### 2.2 Review most common article categories for each year
+
+# %%
+top10_across_years = disc.get_top_n_categories(articles_by_year)
+print(f"The top 10 categories of articles in 2019 were:\n{top10_across_years['2019']}")
+
 
 # %% [markdown]
 # ## 3. Cleaning and pre-processing article text
 
-#%%
+# %%
 # 3.1 Extracting content from specified html tags to avoid irrelevant in-text links 
 # to other articles
 
 article_segments = disc.get_text_segments(articles, TAGS)
 article_text = [' '.join(segment) for segment in article_segments]
 
-#%%
+# %%
 # 3.2 Cleaning content
 
 # This involves minimal cleaning (keeping punctuation and stopwords, no lemmatisation)
 clean_article_text = [tcu.clean_text_minimal(article) for article in article_text]
 
-#%%
+# %%
 # 3.3 Extracting sentences
 # nlp = spacy.load("en_core_web_sm")
 
 processed_articles = [nlp(article) for article in clean_article_text]
 
-#%%
+# %%
 # To do: persist article sentences, as they form the corpus for further analysis
 article_sentences = [[sent.text for sent in article.sents] for article in processed_articles]
 
@@ -96,11 +115,11 @@ with open(os.path.join(DISC_OUTPUTS_DIR, 'article_sentences.pkl'), "wb") as outf
 
 flat_article_sentences = [item for sublist in article_sentences for item in sublist]
 flat_article_sentences = list(set(flat_article_sentences))
-#%%
+# %%
 # Relevant articles
 sentence_mentions = [[sent for sent in art if search_term in sent] for art in article_sentences]
 flat_sentence_mentions = [item for sublist in sentence_mentions for item in sublist]
-#%%
+# %%
 # 3.4 Extract noun chunks related to search term
 noun_chunks = []
 for article in processed_articles:
@@ -111,13 +130,13 @@ for article in processed_articles:
 noun_chunks_str = [str(elem) for elem in noun_chunks]
 dedup_noun_chunks = list(set(noun_chunks_str))
 
-#%%
+# %%
 # 3.5 Tokenize and extract ngrams
 # Filter out stopwords, punctuation, etc. and certain entities
 # Use sentence as a unit of analysis
 tokenised = [tpu.process_text_disc(doc) for doc in nlp.pipe(flat_article_sentences)]
 
-#%%
+# %%
 # Extract ngrams
 count_model = CountVectorizer(tokenizer = disc.identity_tokenizer,
                               lowercase = False, 
@@ -138,7 +157,7 @@ count_dict = dict(zip(names,count_list))
 # ngrams = np.sum(Xc.todense())/2
 search_index = names.index(search_term)
 
-#%%
+# %%
 # Revisited calculation of PMI defining context as a sentence
 pmis = {}
 for ix, name in enumerate(names):
@@ -153,8 +172,13 @@ pruned_pmis = {k:v for k,v in pmis.items() if v >0}
 
 # %%
 # 3.6 Identiy noun chunk associations
+# Remove determiners in noun chunks to increase number of matches
+dedup_noun_chunks = [elem.replace(' a ', ' ').replace(' the ',' ') for elem in dedup_noun_chunks]
+dedup_noun_chunks = [elem.strip() for elem in dedup_noun_chunks]
+
+
 pruned_noun_chunks = [elem for elem in dedup_noun_chunks if elem in names]
-pruned_noun_chunks = [elem for elem in pruned_noun_chunks if count_dict.get(elem, 0) > 5]
+pruned_noun_chunks = [elem for elem in pruned_noun_chunks if count_dict.get(elem, 0) > 3]
 
 
 chunk_pmi = {chunk: pruned_pmis.get(chunk, 0) for chunk in pruned_noun_chunks}
@@ -180,7 +204,7 @@ for ix, row in sentence_sentiment.iterrows():
         # print(noun_chunk)
         if noun_chunk in row['sentences']:
             noun_chunk_sentiments[noun_chunk].append(row['compound'])
-            
+
 # %%
 # Calculate average sentiment for noun chunks
 noun_chunk_agg_sent = {k: np.mean(v) for k,v in noun_chunk_sentiments.items()}
@@ -189,7 +213,7 @@ sorted_sent = sorted(noun_chunk_agg_sent.items(),
                      key = lambda x: x[1], 
                      reverse = True)
 
-# %% Analyse presence of noun chunks in positive and negative sentences
+# %%
 prop_sent = {k: disc.prop_pos(v) for k,v in \
              noun_chunk_sentiments.items()}
-    
+
