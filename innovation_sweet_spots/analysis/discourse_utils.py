7c24474faef5359371f986f38c9aa6f9195c12a2
djyldyz <djyldyz@users.noreply.github.com>
2021-07-31 16:47:56 +0100

    Add function to calculate normalised rank.

--- innovation_sweet_spots/analysis/discourse_utils.py
+++ innovation_sweet_spots/analysis/discourse_utils.py
@@ -14,6 +14,11 @@ import pandas as pd
 from collections import Counter
 from innovation_sweet_spots.utils import text_cleaning_utils as tcu
 from innovation_sweet_spots.utils import text_pre_processing as tpu
+from innovation_sweet_spots.analysis import analysis_utils as iss
+
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
 
 
 DEF_LANGUAGE_MODEL = {"model": "en_core_web_sm", "disable": ["tok2vec"]}
@@ -108,7 +113,7 @@ def prop_pos(sentiment_score_list):
     return (prop_pos, prop_neut, prop_neg)
 
 
-def define_context(sentence, options = 'sentence', n_words = None, 
+def define_context(sentence, options, n_words = None, 
                    search_term = None, nlp_model = None):
     """
     Specify context for sentiment analysis. 
@@ -119,17 +124,16 @@ def define_context(sentence, options = 'sentence', n_words = None,
 
     """
 
-    if options == 'sentence':
-        result = sentence
         
-    elif options == 'n_words':
-        result = get_window(sentence, n_words, search_term)
+    if options == 'n_words':
+        return get_window(sentence, n_words, search_term)
         
     elif options == 'phrase':
-        result = get_phrase(sentence, search_term, nlp_model)
+        return get_phrase(sentence, search_term, nlp_model)
+        
+    elif options == 'sentence':
+        return sentence
     
-    return result
-
 
 def get_window(sentence, n_words, search_term):
     sentence = sentence.replace(' a ', ' ').replace(' the ',' ')
@@ -143,6 +147,8 @@ def get_window(sentence, n_words, search_term):
 
 
 def get_phrase(sentence, search_term, nlp_model):
+    if nlp_model is None:
+        nlp_model = tpu.setup_spacy_model(DEF_LANGUAGE_MODEL)
     matches = []
     for token in nlp_model(sentence):
         if token.pos_ in ['NOUN', 'ADJ', 'VERB']:
@@ -205,26 +211,113 @@ def generate_sentence_corpus(articles, nlp = None):
     clean_article_text = [tcu.clean_text_minimal(article) for article in articles]
     spacy_docs = [nlp(article) for article in clean_article_text]
     article_sentences = [[sent.text for sent in article.sents] for article in spacy_docs]
-    return article_sentences
+    return article_sentences, spacy_docs
 
 
 def remove_determiner_articles(text):
-    replacements = {'a ': ' ', 
-                    ' an ': ' ',
-                    ' the ', ' '}
-    for art, rep in replacements.items():
-        text = text.replace(art, rep)
-        return text
+    # replacements = {' a ': ' ', 
+    #                 ' an ': ' ',
+    #                 ' the ': ' '}
+    # for art, rep in replacements.items():
+    #     text = text.replace(art, rep)
+    #     return text
+    if text[:2] == 'a ':
+        text = text.lstrip('a').lstrip()
+    elif text[:2] == 'an':
+        text = text.lstrip('an').lstrip()
+    elif text[:2] == 'th':
+        text = text.lstrip('the').lstrip()
+    return text
+
 
 
 # To do: catch issues around inherent sentiment in VADER (e.g. lower emissions)
 def calculate_sentiment(sentence_collection, search_term, context, n_words = None, 
                         nlp_model = None):
-    if nlp_model is None:
-        nlp_model = tpu.setup_spacy_model(DEF_LANGUAGE_MODEL)
-    sentences_with_term = [[sent for sent in art if search_term in sent] for art in sentence_collection]
-    sentences_with_term = [item for sublist in sentences_with_term for item in sublist]
     target_context = [define_context(sentence, context, n_words, search_term, nlp_model) for\
-                       sentence in sentences_with_term]
+                       sentence in sentence_collection]
     context_sentiment = iss.get_sentence_sentiment(target_context)
     return context_sentiment
+
+
+# Addressing determiner articles, but not stopwords
+def get_noun_chunks(spacy_corpus, nlp_model, remove_det_articles = True):
+    noun_chunks = []
+    for article in spacy_corpus:
+        for chunk in article.noun_chunks:
+            noun_chunks.append(chunk)
+            # print(chunk)
+    
+    noun_chunks_str = [str(elem) for elem in noun_chunks]
+    if remove_det_articles:
+        noun_chunks_str = [remove_determiner_articles(elem) for elem in noun_chunks_str]
+    dedup_noun_chunks = list(set(noun_chunks_str))
+    return dedup_noun_chunks
+
+
+# Filter out stopwords, punctuation, etc. and certain entities
+def get_spacy_tokens(sentence_collection, nlp_model):
+    tokenised = [tpu.process_text_disc(doc) for doc in nlp_model.pipe(sentence_collection)]
+    return tokenised
+    
+
+def get_ngrams(spacy_tokens, token_range, min_mentions):
+    count_model = CountVectorizer(tokenizer = identity_tokenizer,
+                              lowercase = False, 
+                              ngram_range = token_range,
+                              min_df = min_mentions) # default unigram model
+    doc_term_m = count_model.fit_transform(spacy_tokens)
+    cooccurrence_m = (doc_term_m.T * doc_term_m) # this is co-occurrence matrix in sparse csr format
+    cooccurrence_m.setdiag(0)
+    
+    vocab = count_model.vocabulary_
+    names = count_model.get_feature_names()
+    count_list = doc_term_m.toarray().sum(axis=0)
+    count_dict = dict(zip(names,count_list))
+    
+    return cooccurrence_m, doc_term_m, names, count_dict
+
+
+def calculate_positive_pmi(cooccurrence_matrix, doc_term_m, token_names, token_counts, search_term):
+    search_index = token_names.index(search_term)
+    pmis = {}
+    for ix, name in enumerate(token_names):
+        association = pmi(np.sum(doc_term_m[:, search_index]),
+                      np.sum(doc_term_m[:, ix]),
+                      cooccurrence_matrix[search_index, ix],
+                      doc_term_m.shape[0],
+                      doc_term_m.shape[0])
+        pmis[name] = association
+        
+    pruned_pmis = {k:v for k,v in pmis.items() if v >0}
+    return pruned_pmis
+    
+
+def get_related_terms(noun_chunks, pmi, token_names, token_counts, min_mentions):
+    pruned_noun_chunks = [elem for elem in noun_chunks if elem in token_names]
+    pruned_noun_chunks = [elem for elem in pruned_noun_chunks if token_counts.get(elem, 0) > 3]
+
+
+    chunk_pmi = {chunk: pmi.get(chunk, 0) for chunk in pruned_noun_chunks}
+    chunk_pmi = {k:v for k,v in chunk_pmi.items() if v >0}
+    
+    return chunk_pmi
+
+
+def get_normalised_rank(cooccurrence_m, token_names, token_counts, search_term, 
+                        threshold =1):
+    count_rank = dict()
+    search_index = token_names.index(search_term)
+    total_word_set = np.count_nonzero(cooccurrence_m[search_index,:].toarray())
+    for ix, name in enumerate(token_names):
+        if token_counts[name] > threshold:
+            cooccurence_frequency = cooccurrence_m[search_index, ix]
+            if cooccurence_frequency < 1:
+                continue
+            else:
+                count_rank[name] = cooccurence_frequency
+    count_rank_items = sorted(count_rank.items(), key = lambda x: x[1], reverse = True)
+    normalised_rank = {name: ix/total_word_set for ix, name in enumerate(count_rank_items)}
+    return normalised_rank
+
+    
