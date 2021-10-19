06baca3ad0f13aa15347b26e92bc8279ec613a41
djyldyz <djyldyz@users.noreply.github.com>
2021-10-19 14:35:17 +0100

    Fixed docstring style, removed typing hints, replaced asserts with exceptions.

--- innovation_sweet_spots/analysis/prototyping/public_discourse_analysis/pd_data_collection_utils.py
+++ innovation_sweet_spots/analysis/prototyping/public_discourse_analysis/pd_data_collection_utils.py
@@ -15,51 +15,52 @@ from typing import Iterator
 
 
 def filter_by_category(aggregated_articles, category_list, field='sectionName'):
+    """Returns a subset of articles that fall under specified category.
+    
+    Args:
+        aggregated_articles: A list of articles (each a JSON nested dict).
+        field: A string that specifies the field containing information on article 
+            category. Default value is 'sectionName'.
+        category_list: A list of category names.
+
+    Returns:
+        A list of filtered articles.
+    
+    Raises:
+        TypeError: If aggregated_articles and category_list are not of type list.
+            And field is not of type str.
     """
-    From a list of articles only retain those that fall under specifide category.
-
-    Parameters
-    ----------
-    aggregated_articles (list): list of articles (each a JSON nested dict).
-    field (str): specific name of a field that refers to article category. 
-                default value is 'sectionName'.
-    category_list (list): list of category names (str).
-
-    Returns
-    -------
-    filtered_articles (list)
-
-    """
-    assert isinstance(aggregated_articles, list), 'parameter aggregated_articles={} not of <class "list">'.\
-        format(aggregated_articles)
-    assert isinstance(field, str), 'parameter field={} not of <class "str">'.\
-        format(field)
-    assert isinstance(category_list, list), 'parameter category_list={} not of <class "list">'.\
-        format(category_list)
+    if not isinstance(aggregated_articles, list):
+        raise TypeError, 'parameter aggregated_articles={} not of <class "list">'
+    if not isinstance(category_list, list):
+        raise TypeError, 'parameter category_list={} not of <class "list">'
+    if not isinstance(field, str):
+        raise TypeError, 'parameter field={} not of <class "str">'   
+        
     filtered_articles = [a for a in aggregated_articles if a[field] in category_list]
     return filtered_articles
     
 
-def sort_by_year(filtered_articles, date_field = 'webPublicationDate'):
+def sort_by_year(filtered_articles, date_field='webPublicationDate'):
+    """Sorts articles and groups them by year.
+
+    Args:
+        filtered_articles: A list of articles (each a JSON nested dict).
+        date_field: A string that corresponds to the field containing information
+            about article date. Default is 'webPublicationDate'.
+
+    Returns:
+        A dict mapping years to the corresponding lists of articles.
+    
+    Raises:
+        TypeError: If filtered_articles is not of type list and date_field is not
+            of type str.
     """
-    Sort articles and group by year.
-
-    Parameters
-    ----------
-    filtered_articles (list): list of articles (each a JSON nested dict).
-    date_field (str): field that corresponds to article date. default is 
-                        'webPublicationDate'.
-
-    Returns
-    -------
-    articles_by_year (dict): year as key, list of articles as values.
-                            articles grouped by year using itertools.
-
-    """
-    assert isinstance(filtered_articles, list), 'parameter filtered_articles={} not of <class "list">'.\
-        format(filtered_articles)
-    assert isinstance(date_field, str), 'parameter date_field={} not of <class "str">'.\
-        format(date_field)   
+    if not isinstance(filtered_articles, list):
+        raise TypeError, 'parameter filtered_articles={} not of <class "list">'
+    if not isinstance(date_field, str):
+        raise TypeError, 'parameter date_field={} not of <class "str">'
+        
     sorted_articles = sorted(filtered_articles, key = lambda x: x[date_field][:4])
     articles_by_year = defaultdict(list)
     for k,v in groupby(sorted_articles,key=lambda x:x[date_field][:4]):
@@ -67,59 +68,53 @@ def sort_by_year(filtered_articles, date_field = 'webPublicationDate'):
     return articles_by_year
         
 
-def extract_text_from_html(html, tags: Iterator[str]) -> Iterator[str]:
+def extract_text_from_html(html, tags):
+    """Extracts text from specified html tags.
+
+    Args:
+        html: A content of an article in html format.
+        tags: A list of tags to extract content from.
+
+    Returns:
+        A list of chunks (e.g. paragraphs) of text extracted from the tags.
+        
+    Raises:
+        TypeError: If html is not of type str and tags is not of type list.
     """
-    Extract text from specified html tags.
-
-    Parameters
-    ----------
-    html (str): content of an article in html format.
-    tags (list): list of tags to extract content from.
-
-    Returns
-    -------
-    no_html (list): list of chunks (e.g. paragraphs) of text extracted from the tags.
-
-    """
+    if not isinstance(html, str):
+        raise TypeError, 'parameter html={} not of <class "str">'
+    if not isinstance(tags, list):
+        raise TypeError, 'parameter tags={} not of <class "list">'
+        
     segments = BeautifulSoup(html, "html.parser").find_all(tags)
     no_html = [seg.get_text() for seg in segments]
     return no_html
 
 
-def get_text_segments(articles: Iterator[dict], tags: Iterator[str]) -> Iterator[str]:
-    """
-    Extract segments of article text from Guardian articles in html format.
+def get_text_segments(articles, tags):
+    """Extracts segments of article text from Guardian articles in html format.
 
-    Parameters
-    ----------
-    articles (list): list of dicts containing result of the Guardian
-    search_content function.
-    tags (list): list of tags to extract content from.
+    Args:
+        articles: A list of dicts containing article content.
+        tags: A list of html tags to extract content from.
 
-    Returns
-    -------
-    clean_segments (list): nested list of article segments.
-
+    Returns:
+        A nested list of article segments.
     """
     article_html = [article["fields"]["body"] for article in articles]
     clean_segments = [extract_text_from_html(article, tags) for article in article_html]
     return clean_segments
 
 
-def get_article_text(articles: Iterator[dict], tags: Iterator[str]):
-    """
-    Extract full text of an article from Guardian articles in html format.
+def get_article_text(articles, tags):
+    """Extracts full text of an article from Guardian articles in html format.
 
-    Parameters
-    ----------
-    articles (list): list of dicts containing result of the Guardian
-    search_content function.
-    tags (list): list of tags to extract content from.
+    Args:
+        articles: A list of dicts containing article content.
+        tags: A list of tags to extract content from.
 
-    Returns
-    -------
-    article_text (list): list of article text.
-
+    Returns:
+        A list that contains full text of each article.
     """
     article_segments = get_text_segments(articles, tags)
     article_text = [" ".join(segment) for segment in article_segments]
@@ -127,20 +122,15 @@ def get_article_text(articles: Iterator[dict], tags: Iterator[str]):
 
 
 def get_article_metadata(grouped_articles, fields_to_extract=["id"]):
-    """
-    Extract useful article fields from raw data returned by the Guardian API.
+    """Extracts useful article fields from raw data returned by the Guardian API.
 
-    Parameters
-    ----------
-    grouped_articles (dict): articles grouped by year using itertools.
-    fields_to_extract (list): list of fields. the default is "id", other useful
-                                fields are "webUrl" and "webTitle"
+    Args:
+        grouped_articles: Articles grouped by year using itertools.
+        fields_to_extract: A list of fields to extract data from. The default 
+            is "id", other useful fields are "webUrl" and "webTitle"
 
-    Returns
-    -------
-    year_article_df_combined (pandas.core.frame.DataFrame): dataframe with
-    content of article fields.
-
+    Returns:
+        A pandas dataframe with content of article fields.
     """
     year_article_dfs = []
     for year, articles in grouped_articles.items():
@@ -157,18 +147,14 @@ def get_article_metadata(grouped_articles, fields_to_extract=["id"]):
 
 
 def get_article_text_df(grouped_articles, tags):
-    """
-    Extract article text from articles grouped by year. Deduplicate articles.
+    """Extracts article text, deduplicates articles and organises them by year.
 
-    Parameters
-    ----------
-    grouped_articles (dict): articles grouped by year using itertools.
-    tags (list): specified html tags.
+    Args:
+        grouped_articles: A dict with articles mapped to years using itertools.
+        tags: A list of html tags to extract content from.
 
-    Returns
-    -------
-    year_articles (pandas.core.frame.DataFrame): dataframe with article text, id and year.
-
+    Returns:
+        A pandas dataframe with article text, id and year.
     """
     year_articles_across_years = []
     for year, articles in grouped_articles.items():
@@ -181,24 +167,20 @@ def get_article_text_df(grouped_articles, tags):
     return year_articles
 
 
-def subset_articles(article_text, refine_terms, required_terms, text_field = 'text'):
-    """
-    This function is used to extract a subset of articles that contain specific terms.
+def subset_articles(article_text, refine_terms, required_terms, text_field='text'):
+    """Extracts a subset of articles that contain specific terms.
+    
     It is useful for removing irrelevant articles in instances when a broad general 
     search term is used during data collection, e.g. `hydrogen`.
     
+    Args:
+        article_text: A pandas dataframe that contains article text and id.
+        refine_terms: A list of terms that we use to disambiguate the general term.
+        required_terms: A list of terms. An article must mention at least one of these terms.
+        text_field: A string referring to the name of the column with text, the default is 'text'.
 
-    Parameters
-    ----------
-    article_text (pandas.core.frame.DataFrame): dataframe that contains article text and id.
-    refine_terms (list): list of terms that we use to disambiguate the general term.
-    required_terms (list): an article must mention at least one of these terms.
-    text_field (str): name of the column with text, the default is 'text'.
-
-    Returns
-    -------
-    deduplicated_df (pandas.core.frame.DataFrame): dataframe with a subset of articles.
-
+    Returns:
+        A pandas dataframe with a subset of articles.
     """
     # we first generate a 'thematic' subset of articles that are relevant
     # e.g. applications of hydrogen to heating
@@ -228,20 +210,16 @@ def subset_articles(article_text, refine_terms, required_terms, text_field = 'te
     return deduplicated_df 
 
 
-def remove_articles(article_text, term: str, text_field = 'text'):
-    """
-    A wrapper around pandas subsetting. This function is used to explicitely
-    remove any articles that mention a particular term ('e.g. hydrogen peroxide).
+def remove_articles(article_text, term, text_field='text'):
+    """Explicitely removes any articles that mention a particular term.
 
-    Parameters
-    ----------
-    article_text (pandas.core.frame.DataFrame): dataframe that contains article text and id.
-    term (str): term that we don't want to be mentioned in an article
-    text_field (str): name of the column with text, the default is 'text'.
+    Args:
+        article_text: A dataframe that contains article text and id.
+        term: A string referring to the term that we don't want to be mentioned in an article
+        text_field: A string referring to the name of the column with text, the default is 'text'.
 
-    Returns
-    -------
-    article_text (pandas.core.frame.DataFrame): dataframe with a subset of articles.
+    Returns:
+        A pandas dataframe with a subset of articles.
 
     """
     article_text = article_text[~article_text[text_field].str.contains(term)]
