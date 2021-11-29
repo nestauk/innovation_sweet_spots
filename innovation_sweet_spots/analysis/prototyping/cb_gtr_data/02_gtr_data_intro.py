86ea04d1e8c09b56115652283be76b094b8f889c
beingkk <beingkk@users.noreply.github.com>
2021-11-29 12:35:42 +0000

    gtr data intro

--- innovation_sweet_spots/analysis/prototyping/cb_gtr_data/02_gtr_data_intro.py
+++ innovation_sweet_spots/analysis/prototyping/cb_gtr_data/02_gtr_data_intro.py
@@ -17,10 +17,62 @@
 # %% [markdown]
 # # GtR data exploration
 # - Load data
+# - Check organisations participating in a particular project
 #
 # Prerequisite: Fetching GtR data by running `make fetch-daps1` from the main repo directory
+
+# %% [markdown]
+# ## Import data and dependencies
 
 # %%
 from innovation_sweet_spots.getters import gtr
 import innovation_sweet_spots.analysis.analysis_utils as iss
 import altair as alt
+
+# %%
+import importlib
+
+importlib.reload(gtr)
+
+# %%
+## Import GtR data
+
+# Projects
+gtr_projects = gtr.get_gtr_projects()
+# Funding (with start and end dates)
+gtr_funds = gtr.get_gtr_funds()
+# Organisations
+gtr_organisations = gtr.get_gtr_organisations()
+# Research topics
+gtr_topics = gtr.get_gtr_topics()
+
+# Links table between projects and organisations
+link_gtr_organisations = gtr.get_link_table("gtr_organisations")
+# Links table between projects and research topics
+link_gtr_topics = gtr.get_link_table("gtr_topic")
+# Links between projects and funds (might be useful to find end-dates of project funding)
+link_gtr_funds = gtr.get_link_table("gtr_funds")
+
+# Add funding data to projects (note, I'm using another funding table which
+# I've obtained directly via GtR API and is less ambiguous)
+gtr_project_funds = gtr.add_funding_data(gtr_projects, gtr.get_gtr_funds_api())
+# Add organisation data to projects
+gtr_project_organisations = iss.link_gtr_projects_and_orgs(
+    gtr_organisations, link_gtr_organisations
+)
+
+# %%
+gtr_projects.head(5)
+
+# %% [markdown]
+# ## Check a particular project
+
+# %%
+check_project = "106DA39B-4A30-4D98-BB8B-42779D7CEE41"
+
+# %%
+gtr_project_funds[gtr_project_funds.project_id == check_project]
+
+# %%
+# Organisations participating in the project
+gtr_project_organisations.query(f"project_id=='{check_project}'")
--- innovation_sweet_spots/getters/gtr.py
+++ innovation_sweet_spots/getters/gtr.py
@@ -17,11 +17,27 @@ def get_gtr_funds():
     return pd.read_csv(f"{GTR_PATH}/gtr_funds.csv")
 
 
-def get_gtr_topics():
+def get_gtr_funds_api() -> pd.DataFrame:
+    """
+    GtR project funding data that has been fetched directly using GtR API.
+    This was done, as the database's funding data was ambiguous.
+    Contains three columns ['index_col', 'project_id', 'amount'], with amount in GBP.
+    """
+    df = (
+        pd.read_csv(
+            f"{GTR_PATH}/gtr_funds_api.csv", names=["index_col", "project_id", "amount"]
+        )
+        # Remove the index column, which is redundant
+        .drop("index_col", axis=1)
+    )
+    return df
+
+
+def get_gtr_topics() -> pd.DataFrame:
     return pd.read_csv(f"{GTR_PATH}/gtr_topics.csv")
 
 
-def get_gtr_organisations():
+def get_gtr_organisations() -> pd.DataFrame:
     return pd.read_csv(f"{GTR_PATH}/gtr_organisations.csv")
 
 
@@ -56,7 +72,33 @@ def pullout_gtr_links(tables: Iterable[str]):
         )
 
 
-def get_cleaned_project_texts():
+def get_cleaned_project_texts() -> pd.DataFrame:
     df = pd.read_csv(f"{OUTPUT_GTR_PATH}/gtr_project_clean_text.csv")
     df["project_text"] = df["project_text"].apply(lambda x: literal_eval(x))
     return df
+
+
+def add_funding_data(
+    gtr_docs: pd.DataFrame, gtr_funds: pd.DataFrame, project_id_column="project_id"
+) -> pd.DataFrame:
+    """
+    Update research project funding column with funds
+    """
+    gtr_docs = gtr_docs.copy()
+    # If amount column already exists, keep it as amount_old
+    if "amount" in gtr_docs.columns:
+        gtr_docs["amount_old"] = gtr_docs["amount"].copy()
+        gtr_docs.drop("amount", axis=1)
+    # Add the provided funding data
+    gtr_docs = gtr_docs.merge(
+        gtr_funds[[project_id_column, "amount"]], on=project_id_column, how="left"
+    )
+    # Check if there are projects with missing funding data (i.e. no corresponding
+    # project in gtr_funds dataframe) and impute with 0
+    missing_amount = gtr_docs.amount.isnull()
+    n_missing = missing_amount.sum()
+    logging.info(f"{n_missing} documents without funding info")
+    if n_missing > 0:
+        gtr_docs.loc[missing_amount, "funding_category"] = "NO_FUND_INFO"
+        gtr_docs["amount"] = gtr_docs["amount"].fillna(0)
+    return gtr_docs
