bedfd6b2f3371daa3d3c76eac8f57a6318622268
beingkk <beingkk@users.noreply.github.com>
2022-02-04 14:17:37 +0000

    first commit

---
+++ innovation_sweet_spots/analysis/examples/07_query_labels.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     comment_magics: true
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Selecting projects and companies by keywords and key phrases
+
+# %%
+import innovation_sweet_spots.analysis.wrangling_utils as wu
+import pandas as pd
+
+GTR = wu.GtrWrangler()
+CB = wu.CrunchbaseWrangler()
+
+# %%
+SEARCH_LABELS = [
+    ["label1"],
+]
+
+# %% [markdown]
+# # Check research projects
+
+# %%
+
+# %% [markdown]
+# # Check companies
+
+# %%
+
+# %%
---
+++ innovation_sweet_spots/analysis/query_labels.py
@@ -0,0 +1,5 @@
+"""
+innovation_sweet_spots.analysis.query_labels
+
+Module for selecting Crunchbase and GtR data using the existing data labels
+"""
