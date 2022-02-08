6f88839b42abde63ac5dd1ccf6161a8d74904561
beingkk <beingkk@users.noreply.github.com>
2022-02-08 14:08:13 +0000

    first commit

---
+++ innovation_sweet_spots/analysis/query_lda_model.py
@@ -0,0 +1,10 @@
+"""
+innovation_sweet_spots.analysis.query_lda_model
+
+Module for selecting items using LDA topic model
+"""
+# from innovation_sweet_spots import logging
+# import numpy as np
+# import pandas as pd
+# from typing import Iterator, Dict, Union
+# from numpy.typing import ArrayLike
