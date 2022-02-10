35b34bf8e069cdfb28555ed2ba1c8eb96d469a29
beingkk <beingkk@users.noreply.github.com>
2022-02-10 09:18:48 +0000

    factoring topic helper functions

--- innovation_sweet_spots/analysis/query_lda_model.py
+++ innovation_sweet_spots/analysis/query_lda_model.py
@@ -8,3 +8,24 @@ Module for selecting items using LDA topic model
 # import pandas as pd
 # from typing import Iterator, Dict, Union
 # from numpy.typing import ArrayLike
+
+# Model getter (maybe move into model)
+def get_pilot_lda_model():
+    """
+    mdl = tp.LDAModel.load(str(PROJECT_DIR / "outputs/models/full_corpus_lda_model.bin"))
+    Returns:
+        A dictionary with 'the model' and a table with topic definitions/keywords
+    """
+    pass
+
+
+def get_topic_probabilities():
+    """ """
+    pass
+
+
+def query_topics(topics: Iterator[int], topic_model) -> pd.DataFrame:
+    pass
+
+
+#
