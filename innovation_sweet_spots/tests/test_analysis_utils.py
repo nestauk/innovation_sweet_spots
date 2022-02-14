1d8761adadef7ceb0a0c9c20c7f225db5832b392
Jack Rasala <jack.rasala@nesta.org.uk>
2022-02-14 14:52:14 +0000

    Update test_analysis_utils.py

--- innovation_sweet_spots/tests/test_analysis_utils.py
+++ innovation_sweet_spots/tests/test_analysis_utils.py
@@ -4,29 +4,48 @@ import pandas as pd
 from pandas._testing import assert_frame_equal, assert_series_equal
 
 
-def test_impute_empty_years():
-    mock_input_data = {"year": [2000, 2002], "values": [1, 1]}
-    mock_df = pd.DataFrame(mock_input_data)
+def test_impute_empty_periods():
+    mock_df = pd.DataFrame(
+        {
+            "time_period": ["2000-01-01", "2002-01-01"],
+            "values": [1.0, 1.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
 
-    df = impute_empty_years(mock_df)
-    imputed_data = {"year": [2000, 2001, 2002], "values": [1, 0, 1]}
-    assert_frame_equal(df, pd.DataFrame(imputed_data))
+    df = impute_empty_periods(mock_df, "time_period", "Y", 2000, 2002)
+    imputed_data = pd.DataFrame(
+        {
+            "time_period": ["2000-01-01", "2001-01-01", "2002-01-01"],
+            "values": [1.0, 0.0, 1.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert df.equals(imputed_data)
 
-    df = impute_empty_years(mock_df, min_year=1999)
-    imputed_data = {"year": [1999, 2000, 2001, 2002], "values": [0, 1, 0, 1]}
-    assert_frame_equal(df, pd.DataFrame(imputed_data))
+    df = impute_empty_periods(mock_df, "time_period", "Y", 1999, 2002)
+    imputed_data = pd.DataFrame(
+        {
+            "time_period": ["1999-01-01", "2000-01-01", "2001-01-01", "2002-01-01"],
+            "values": [0.0, 1.0, 0.0, 1.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert df.equals(imputed_data)
 
-    df = impute_empty_years(mock_df, min_year=1999, max_year=2004)
-    imputed_data = {"year": list(range(1999, 2005)), "values": [0, 1, 0, 1, 0, 0]}
-    assert_frame_equal(df, pd.DataFrame(imputed_data))
-
-
-def test_set_def_min_max_years():
-    mock_input_data = {"year": [2000, 2001, 2002], "values": [1, 1, 1]}
-    mock_df = pd.DataFrame(mock_input_data)
-    assert set_def_min_max_years(mock_df, min_year=None, max_year=None) == (2000, 2002)
-    assert set_def_min_max_years(mock_df, min_year=2001, max_year=None) == (2001, 2002)
-    assert set_def_min_max_years(mock_df, min_year=2001, max_year=2001) == (2001, 2001)
+    df = impute_empty_periods(mock_df, "time_period", "Y", 1999, 2005)
+    imputed_data = pd.DataFrame(
+        {
+            "time_period": [
+                "1999-01-01",
+                "2000-01-01",
+                "2001-01-01",
+                "2002-01-01",
+                "2003-01-01",
+                "2004-01-01",
+                "2005-01-01",
+            ],
+            "values": [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert_frame_equal(df, imputed_data)
 
 
 def test_gtr_deduplicate_projects():
@@ -47,146 +66,116 @@ def test_gtr_deduplicate_projects():
     assert_frame_equal(df, pd.DataFrame(deduplicated_data))
 
 
-def test_gtr_funding_per_year():
+def test_gtr_funding_per_period():
     mock_input_data = {
         "start": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
         "project_id": ["a", "b", "c", "d"],
         "amount": [1000, 2000, 10000, 5000],
     }
     mock_df = pd.DataFrame(mock_input_data)
-    aggregated_df = gtr_funding_per_year(mock_df)
-    aggregated_data = {
-        "year": [2001, 2002, 2003, 2004],
-        "no_of_projects": [1, 2, 0, 1],
-        "amount_total": [1.0, 12.0, 0.0, 5.0],
-        "amount_median": [1.0, 6.0, 0.0, 5.0],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
-    # Set lower bound for years
-    aggregated_df = gtr_funding_per_year(mock_df, min_year=2002)
-    aggregated_data = {
-        "year": [2002, 2003, 2004],
-        "no_of_projects": [2, 0, 1],
-        "amount_total": [12.0, 0.0, 5.0],
-        "amount_median": [6.0, 0.0, 5.0],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
-    # Set lower and higher bound for years
-    aggregated_df = gtr_funding_per_year(mock_df, min_year=2002, max_year=2003)
-    aggregated_data = {
-        "year": [2002, 2003],
-        "no_of_projects": [2, 0],
-        "amount_total": [12.0, 0.0],
-        "amount_median": [6.0, 0.0],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
+    aggregated_df = gtr_funding_per_period(
+        mock_df, period="Y", min_year=2001, max_year=2004
+    )
+    aggregated_data = pd.DataFrame(
+        {
+            "time_period": ["2001-01-01", "2002-01-01", "2003-01-01", "2004-01-01"],
+            "no_of_projects": [1.0, 2.0, 0.0, 1.0],
+            "amount_total": [1.0, 12.0, 0.0, 5.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert_frame_equal(aggregated_df, aggregated_data)
 
 
-def test_gtr_get_all_timeseries():
-    mock_input_data = {
-        "project_id": ["1", "2", "3", "4"],
-        "title": ["Project A", "Project A", "Project B", "Project C"],
-        "description": ["aaa", "aaa", "bbb", "ccc"],
-        "start": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
-        "amount": [1000, 2000, 10000, 5000],
-    }
-    mock_df = pd.DataFrame(mock_input_data)
-    output = gtr_get_all_timeseries(mock_df)
-    output_data = {
-        "year": [2001, 2002, 2003, 2004],
-        "no_of_projects": [1, 1, 0, 1],
-        "amount_total": [1.0, 12.0, 0.0, 5.0],
-        "amount_median": [1.0, 6.0, 0.0, 5.0],
-    }
-    assert_frame_equal(output, pd.DataFrame(output_data))
+def test_gtr_get_all_timeseries_period():
+    mock_df = pd.DataFrame(
+        {
+            "project_id": ["1", "2", "3", "4"],
+            "title": ["Project A", "Project A", "Project B", "Project C"],
+            "description": ["aaa", "aaa", "bbb", "ccc"],
+            "start": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
+            "amount": [1000, 2000, 10000, 5000],
+        }
+    )
+    output = gtr_get_all_timeseries_period(
+        mock_df, period="year", min_year=2001, max_year=2004
+    )
+    output_data = pd.DataFrame(
+        {
+            "time_period": ["2001-01-01", "2002-01-01", "2003-01-01", "2004-01-01"],
+            "no_of_projects": [1.0, 1.0, 0.0, 1.0],
+            "amount_total": [1.0, 12.0, 0.0, 5.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert_frame_equal(output, output_data)
 
 
-def test_cb_orgs_founded_per_year():
+def test_cb_orgs_founded_per_period():
     mock_input_data = {
         "id": ["a", "b", "c", "d"],
         "founded_on": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
     }
     mock_df = pd.DataFrame(mock_input_data)
-    aggregated_df = cb_orgs_founded_per_year(mock_df)
-    aggregated_data = {
-        "year": [2001, 2002, 2003, 2004],
-        "no_of_orgs_founded": [1, 2, 0, 1],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
-    # Set lower bound for years
-    aggregated_df = cb_orgs_founded_per_year(mock_df, min_year=2002)
-    aggregated_data = {
-        "year": [2002, 2003, 2004],
-        "no_of_orgs_founded": [2, 0, 1],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
-    # Set lower and higher bound for years
-    aggregated_df = cb_orgs_founded_per_year(mock_df, min_year=2002, max_year=2005)
-    aggregated_data = {
-        "year": [2002, 2003, 2004, 2005],
-        "no_of_orgs_founded": [2, 0, 1, 0],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
+    aggregated_df = cb_orgs_founded_per_period(
+        mock_df, period="Y", min_year=2001, max_year=2004
+    )
+    aggregated_data = pd.DataFrame(
+        {
+            "time_period": ["2001-01-01", "2002-01-01", "2003-01-01", "2004-01-01"],
+            "no_of_orgs_founded": [1.0, 2.0, 0.0, 1.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert_frame_equal(aggregated_df, aggregated_data)
 
 
-def test_cb_investments_per_year():
-    mock_input_data = {
-        "funding_round_id": ["a", "b", "c", "d"],
-        "announced_on": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
-        "raised_amount_usd": [10, 20, 30, 50],
-        "raised_amount_gbp": [12, 24, 36, 58],
-    }
-    mock_df = pd.DataFrame(mock_input_data)
-    aggregated_df = cb_investments_per_year(mock_df)
-    aggregated_data = {
-        "year": [2001, 2002, 2003, 2004],
-        "no_of_rounds": [1, 2, 0, 1],
-        "raised_amount_usd_total": [10, 50, 0, 50],
-        "raised_amount_gbp_total": [12, 60, 0, 58],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
-    # Set lower bound for years
-    aggregated_df = cb_investments_per_year(mock_df, min_year=2002)
-    aggregated_data = {
-        "year": [2002, 2003, 2004],
-        "no_of_rounds": [2, 0, 1],
-        "raised_amount_usd_total": [50, 0, 50],
-        "raised_amount_gbp_total": [60, 0, 58],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
-    # Set lower and higher bound for years
-    aggregated_df = cb_investments_per_year(mock_df, min_year=2002, max_year=2005)
-    aggregated_data = {
-        "year": [2002, 2003, 2004, 2005],
-        "no_of_rounds": [2, 0, 1, 0],
-        "raised_amount_usd_total": [50, 0, 50, 0],
-        "raised_amount_gbp_total": [60, 0, 58, 0],
-    }
-    assert_frame_equal(aggregated_df, pd.DataFrame(aggregated_data))
+def test_cb_investments_per_period():
+    mock_df = pd.DataFrame(
+        {
+            "funding_round_id": ["a", "b", "c", "d"],
+            "announced_on": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
+            "raised_amount_usd": [10, 20, 30, 50],
+            "raised_amount_gbp": [12, 24, 36, 58],
+        }
+    )
+    aggregated_df = cb_investments_per_period(mock_df, "Y", 2001, 2004)
+    aggregated_data = pd.DataFrame(
+        {
+            "time_period": ["2001-01-01", "2002-01-01", "2003-01-01", "2004-01-01"],
+            "no_of_rounds": [1.0, 2.0, 0.0, 1.0],
+            "raised_amount_usd_total": [10.0, 50.0, 0.0, 50.0],
+            "raised_amount_gbp_total": [12.0, 60.0, 0.0, 58.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert_frame_equal(aggregated_df, aggregated_data)
 
 
 def test_cb_get_all_timeseries():
-    mock_organisation_data = {
-        "id": ["a", "b", "c", "d"],
-        "founded_on": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
-    }
-    mock_deal_data = {
-        "funding_round_id": ["a", "b", "c", "d"],
-        "announced_on": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
-        "raised_amount_usd": [10, 20, 30, 50],
-        "raised_amount_gbp": [12, 24, 36, 58],
-    }
-    output = cb_get_all_timeseries(
-        pd.DataFrame(mock_organisation_data), pd.DataFrame(mock_deal_data)
+    mock_organisation_data = pd.DataFrame(
+        {
+            "id": ["a", "b", "c", "d"],
+            "founded_on": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
+        }
     )
-    output_data = {
-        "year": [2001, 2002, 2003, 2004],
-        "no_of_rounds": [1, 2, 0, 1],
-        "raised_amount_usd_total": [10, 50, 0, 50],
-        "raised_amount_gbp_total": [12, 60, 0, 58],
-        "no_of_orgs_founded": [1, 2, 0, 1],
-    }
-    assert_frame_equal(output, pd.DataFrame(output_data))
+    mock_deal_data = pd.DataFrame(
+        {
+            "funding_round_id": ["a", "b", "c", "d"],
+            "announced_on": ["2001-02-01", "2002-10-01", "2002-12-05", "2004-04-01"],
+            "raised_amount_usd": [10, 20, 30, 50],
+            "raised_amount_gbp": [12, 24, 36, 58],
+        }
+    )
+    output = cb_get_all_timeseries(
+        mock_organisation_data, mock_deal_data, "year", 2001, 2004
+    )
+    output_data = pd.DataFrame(
+        {
+            "time_period": ["2001-01-01", "2002-01-01", "2003-01-01", "2004-01-01"],
+            "no_of_rounds": [1.0, 2.0, 0.0, 1.0],
+            "raised_amount_usd_total": [10.0, 50.0, 0.0, 50.0],
+            "raised_amount_gbp_total": [12.0, 60.0, 0.0, 58.0],
+            "no_of_orgs_founded": [1.0, 2.0, 0.0, 1.0],
+        }
+    ).astype({"time_period": "datetime64[ns]"})
+    assert_frame_equal(output, output_data)
 
 
 def test_moving_average():
