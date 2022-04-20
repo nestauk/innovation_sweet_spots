085ec138851e3146b0e926ab7bbff91c7af64a73
Jack Rasala <47244945+Jack-0-0@users.noreply.github.com>
2022-04-20 12:34:24 +0200

    [136] Add grants variables to investments predictions dataset (#140)
    Closes #136.
    
    This adds variables to the investments predictions dataset relating to grants:
    * `total_grant_amount_gbp`
    * `n_grants`
    * `has_received_ukri_grant`
    * `has_received_grant`
    * `last_grant_amount_gbp`
    * `n_months_since_last_grant`
    * `n_months_before_first_grant`
    
    Co-authored-by: beingkk <beingkk@users.noreply.github.com>

--- innovation_sweet_spots/pipeline/pilot/investment_predictions/create_dataset/create_dataset.py
+++ innovation_sweet_spots/pipeline/pilot/investment_predictions/create_dataset/create_dataset.py
@@ -5,7 +5,7 @@ used for predicting future investment sucess for companies.
 Run the following command in the terminal to see the options for creating the dataset:
 python innovation_sweet_spots/pipeline/pilot/investment_predictions/create_dataset/create_dataset.py --help
 
-On an M1 macbook it takes ~7 mins to run on the full dataset and ~1 min 30 secs to run in test mode.
+On an M1 macbook it takes ~8 mins 30 secs to run on the full dataset and ~2 mins 30 secs to run in test mode.
 """
 import typer
 from innovation_sweet_spots import PROJECT_DIR
@@ -18,12 +18,17 @@ from innovation_sweet_spots.getters.crunchbase import (
     get_crunchbase_investments,
     get_crunchbase_people,
     get_crunchbase_degrees,
+    get_crunchbase_gtr_lookup,
 )
+from innovation_sweet_spots.getters.gtr import get_link_table
 from innovation_sweet_spots.pipeline.pilot.investment_predictions.create_dataset import (
     utils,
 )
 import pandas as pd
-from innovation_sweet_spots.analysis.wrangling_utils import CrunchbaseWrangler
+from innovation_sweet_spots.analysis.wrangling_utils import (
+    CrunchbaseWrangler,
+    GtrWrangler,
+)
 
 # Adjust the Crunchbase data snapshot path (ideally, should adapt the code to accommodate the newest snapshot)
 crunchbase.CB_PATH = crunchbase.CB_PATH.parents[0] / "cb_2021"
@@ -73,13 +78,25 @@ DROP_COLS = [
     "industry_clean",
     "groups",
     "first_funding_date_in_window",
-    "last_funding_round_in_window",
     "latest_funding_date_in_window",
     "last_funding_id_in_window",
     "org_id_x",
     "org_id_y",
     "org_id",
+    "cb_org_id",
+    "last_funding_id_in_window",
+    "first_grant_date",
+    "last_grant_date",
 ]
+
+UKRI_GRANT_PROVIDERS_TO_FILTER = (
+    "Innovate UK|"
+    "UKTI|"
+    "STFC|"
+    "Biotechnology and Biological Sciences Research Council|"
+    "Engineering and Physical Sciences Research Council|"
+    "Medical Research Council"
+)
 
 
 def create_dataset(
@@ -107,36 +124,106 @@ def create_dataset(
     window_end_date = pd.to_datetime(window_end_date)
     success_start_date = window_end_date
 
+    # Create a CrunchbaseWrangler
+    cb_wrangler = CrunchbaseWrangler()
+
     # Load datasets
     cb_orgs = (
         get_crunchbase_orgs()
         .query("country_code == 'GBR'")
-        .query(f"'{window_start_date}' <= founded_on")
+        .assign(founded_on=lambda x: pd.to_datetime(x.founded_on, errors="coerce"))
         .reset_index()
     )
     if test:
         cb_orgs = cb_orgs.head(2000)
     cb_acquisitions = get_crunchbase_acquisitions()
     cb_ipos = get_crunchbase_ipos()
-    cb_funding_rounds = get_crunchbase_funding_rounds().query(
-        f"'{window_start_date}' <= announced_on"
+    cb_funding_rounds_grants = get_crunchbase_funding_rounds().assign(
+        announced_on=lambda x: pd.to_datetime(x.announced_on, errors="coerce")
     )
+
     cb_investments = get_crunchbase_investments()
     cb_people = get_crunchbase_people()
     cb_degrees = get_crunchbase_degrees()
 
-    # Dedupe descriptions
+    # Convert funding amounts to GBP
+    cb_funding_rounds_grants_gbp = cb_wrangler.convert_deal_currency_to_gbp(
+        funding=cb_funding_rounds_grants.astype({"announced_on": "datetime64[ns]"}),
+        date_column="announced_on",
+        amount_column="raised_amount",
+        usd_amount_column="raised_amount_usd",
+        converted_column="raised_amount_gbp",
+    )
+
+    # Split out crunchbase investment from grants
+    cb_funding_rounds_gbp = cb_funding_rounds_grants_gbp.query(
+        "investment_type != 'grant'"
+    )
+
+    # Split out crunchbase grants from investments
+    cb_grants_gbp = (
+        cb_funding_rounds_grants_gbp.query("investment_type == 'grant'")[
+            ["id", "announced_on", "raised_amount_gbp", "org_id", "org_name"]
+        ]
+        .query(f"'{window_start_date}' <= announced_on <= '{window_end_date}'")
+        .rename(columns={"id": "funding_round_id"})
+    )
+
+    # Remove GtR grants from crunchbase grants
+    cb_grants_without_gtr_grants = utils.remove_gtr_grants_from_cb_grants(
+        cb_grants_gbp=cb_grants_gbp,
+        cb_investments=cb_investments,
+        ukri_grant_providers_to_filter=UKRI_GRANT_PROVIDERS_TO_FILTER,
+    )
+    # Standardise crunchbase grants so it can be combined with gtr grants
+    cb_grants_standardised = utils.standardise_cb_grants(cb_grants_without_gtr_grants)
+
+    # Load gtr_orgs link table and filter data to lead organisations only
+    gtr_lead_orgs_to_project_id_lookup = (
+        get_link_table(table="gtr_organisations")
+        .rename(columns={"id": "gtr_org_id"})
+        .query("rel == 'LEAD_ORG'")
+    )
+    # Create a GtrWrangler
+    gtr_wrangler = GtrWrangler()
+    # Create dataframe of grants with funding amounts and receiving organisation id
+    gtr_grants = utils.gtr_projects_with_lead_orgs(
+        gtr_wrangler,
+        gtr_lead_orgs_to_project_id_lookup,
+        window_start_date,
+        window_end_date,
+    )
+
+    # Load cb to gtr lookup
+    cb_gtr_org_lookup = utils.explode_cb_gtr_lookup(get_crunchbase_gtr_lookup())
+    # Add gtr project info to cb_gtr_lookup
+    cb_gtr_lookup_projects = utils.add_gtr_project_to_cb_gtr_lookup(
+        cb_gtr_org_lookup, gtr_grants
+    )
+    # Standardise gtr grants so it can be combined with crunchbase grants
+    gtr_grants_standardised = utils.standardise_gtr_grants(cb_gtr_lookup_projects)
+    # Join together crunchbase and gtr grants
+    combined_cb_gtr_grants = utils.join_cb_gtr_grants(
+        cb_grants_standardised, gtr_grants_standardised
+    )
+    # Create grants features
+    grants = utils.grants_features(combined_cb_gtr_grants)
+    # Create last_grant_amount_gbp feature
+    latest_grant_amount = utils.last_grant_amount(combined_cb_gtr_grants)
+    # Add last_grant_amount_gbp to grants features
+    grants = grants.merge(
+        right=latest_grant_amount, how="left", left_on="cb_org_id", right_on="cb_org_id"
+    )
+
+    # Dedupe company descriptions
     cb_orgs = cb_orgs.pipe(utils.dedupe_descriptions)
 
-    # Create a CrunchbaseWrangler
-    CB = CrunchbaseWrangler()
-
     # Create dict of industry to wider category groupings
-    industry_to_group_map = CB.industry_to_group
+    industry_to_group_map = cb_wrangler.industry_to_group
     industry_to_group_map["no_industry_listed"] = []
 
     # Add industries
-    inds = CB.get_company_industries(cb_orgs, return_lists=True)
+    inds = cb_wrangler.get_company_industries(cb_orgs, return_lists=True)
     # Rename nan industries
     inds["industry_clean"] = inds["industry"].apply(
         utils.convert_nan_list_to_no_industry_listed
@@ -190,7 +277,7 @@ def create_dataset(
         )
     ).reset_index()
 
-    (
+    dataset = (
         # Add flag for founded on and filter out companies with 0 flag
         cb_orgs.pipe(
             utils.window_flag,
@@ -199,11 +286,11 @@ def create_dataset(
             variable="founded_on",
         )
         .query("founded_on_in_window == 1")
-        # Add additional variables
+        # Add additional features
         .pipe(utils.add_acquired_on, cb_acquisitions)
         .pipe(utils.add_went_public_on, cb_ipos)
-        .pipe(utils.add_funding_round_ids, cb_funding_rounds)
-        .pipe(utils.add_funding_round_dates, cb_funding_rounds)
+        .pipe(utils.add_funding_round_ids, cb_funding_rounds_gbp)
+        .pipe(utils.add_funding_round_dates, cb_funding_rounds_gbp)
         # Add flags for company acquired on and went public in time window
         .pipe(
             utils.window_flag,
@@ -257,33 +344,24 @@ def create_dataset(
             window_end_date,
             "first_funding_date_in_window",
         )
-        # Add col for last funding round number in window
-        .pipe(
-            utils.add_first_last_date_col_number,
-            col_contains_string="funding_round_date",
-            last=True,
-            start_date=window_start_date,
-            end_date=window_end_date,
-            new_col="last_funding_round_in_window",
-        )
-        # Add col for last funding id in window
-        .pipe(utils.add_last_funding_id_in_window)
-        # Add col for last_investment_round_type and last_investment_round_usd
-        .pipe(utils.add_last_investment_round_info, cb_funding_rounds)
-        # Add col for number of months before first investment
-        .pipe(utils.add_n_months_before_first_investment_in_window)
-        # Add col for total investment received
-        .pipe(
-            utils.add_total_investment,
-            cb_funding_rounds,
-            window_start_date,
-            window_end_date,
-        )
         # Add col for number of funding rounds
         .pipe(
             utils.add_n_funding_rounds_in_window,
             start_date=window_start_date,
             end_date=window_end_date,
+        )
+        # Add col for last funding id in window
+        .pipe(utils.add_last_funding_id_in_window)
+        # Add col for last_investment_round_type and last_investment_round_usd
+        .pipe(utils.add_last_investment_round_info, cb_funding_rounds_gbp)
+        # Add col for number of months before first investment
+        .pipe(utils.add_n_months_before_first_investment_in_window)
+        # Add col for total investment received
+        .pipe(
+            utils.add_total_investment,
+            cb_funding_rounds_gbp,
+            window_start_date,
+            window_end_date,
         )
         # Add col for number of months since last investment
         .pipe(
@@ -297,13 +375,19 @@ def create_dataset(
         # Add col for number of unique investors total
         .pipe(
             utils.add_n_unique_investors_total,
-            cb_funding_rounds=cb_funding_rounds,
+            cb_funding_rounds=cb_funding_rounds_gbp,
             cb_investments=cb_investments,
             start_date=window_start_date,
             end_date=window_end_date,
         )
-        # Add cols for founders information
+        # Add cols for founders features
         .pipe(utils.add_founders, org_id_founders)
+        # Add cols for grants features
+        .pipe(utils.add_grants, grants)
+        # Add col for number of months since last grant
+        .pipe(utils.add_n_months_since_last_grant, window_end_date)
+        # Add col for number of before first grant
+        .pipe(utils.add_n_months_before_first_grant)
         # Drop columns
         .pipe(
             utils.drop_multi_cols,
@@ -311,12 +395,13 @@ def create_dataset(
         )
         .drop(columns=DROP_COLS)
         .reset_index(drop=True)
-        # Save to csv
-        .to_csv(
-            PROJECT_DIR
-            / "outputs/finals/pilot_outputs/"
-            / f"investment_predictions/company_data_window_{str(window_start_date).split(' ')[0]}-{str(window_end_date).split(' ')[0]}.csv"
-        )
+    )
+    dataset.to_csv(
+        PROJECT_DIR
+        / "outputs/finals/pilot_outputs"
+        / "investment_predictions/company_data_window_"
+        f"{str(window_start_date).split(' ')[0]}-{str(window_end_date).split(' ')[0]}"
+        ".csv"
     )
 
 
