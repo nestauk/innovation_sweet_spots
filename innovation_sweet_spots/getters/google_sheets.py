5eb9eea20909c6b61d80244ebdb27cd628e89405
Jack Rasala <jack@jrds.io>
2022-11-23 14:07:50 +0900

    Refactor google_sheets getter

--- innovation_sweet_spots/getters/google_sheets.py
+++ innovation_sweet_spots/getters/google_sheets.py
@@ -31,8 +31,18 @@ def get_credentials_path() -> Path:
     return None
 
 
-def gsheet_api_check(scopes: Iterable[str]):
-    """Authorise access to Google Sheets"""
+def gsheet_api_check(
+    scopes: Iterable[str] = ["https://www.googleapis.com/auth/spreadsheets"]
+):
+    """Authorise access to Google Sheets
+
+    Args:
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"] or
+            ["https://www.googleapis.com/auth/spreadsheets"].
+
+    Returns:
+        Google credentials object
+    """
     creds = None
     credentials_path = get_credentials_path()
     token_path = credentials_path.parent / "token.pickle"
@@ -51,136 +61,127 @@ def gsheet_api_check(scopes: Iterable[str]):
     return creds
 
 
-def pull_sheet_data(scopes: Iterable[str], spreadsheet_id: str, data_range: str):
+def get_sheet_from_googlesheets(spreadsheet_id: str, data_range: str) -> list:
     """Get data from Google Sheets"""
-    creds = gsheet_api_check(scopes)
+    creds = gsheet_api_check()
     service = build("sheets", "v4", credentials=creds)
     sheet = service.spreadsheets()
     result = (
         sheet.values().get(spreadsheetId=spreadsheet_id, range=data_range).execute()
     )
-    values = result.get("values", [])
-
-    if not values:
+    values = result.get("values")
+    if values:
+        logging.info("Data copied from googlesheets")
+        return values
+    else:
         logging.warning("No data found.")
+
+
+def get_sheet_data(
+    local_path: Path,
+    spreadsheet_id: str,
+    data_range: str,
+    from_local=True,
+    save_locally=True,
+) -> pd.DataFrame:
+    """Load data from either googlesheets or csv locally
+
+    Args:
+        local_path: Path to csv
+        spreadsheet_id: Googlesheets id, e.g '1O1PA6TvHHVyX1-lMAMoGYUhecQToGQpqv3l9GxXMbB8'
+        data_range: Sheet to load from googlesheet
+        from_local: Set to True to load from local_path
+        save_locally: Set to True to save data loaded from
+            googlesheets to local_path.
+
+    Returns:
+        Dataframe of loaded data
+    """
+    if from_local:
+        logging.info(f"Loading search terms from {local_path}")
+        return pd.read_csv(local_path)
     else:
-        rows = (
-            sheet.values().get(spreadsheetId=spreadsheet_id, range=data_range).execute()
-        )
-        data = rows.get("values")
-        logging.info("COMPLETE: Data copied")
-        return data
+        data = get_sheet_from_googlesheets(spreadsheet_id, data_range)
+        data_df = pd.DataFrame(data[1:], columns=data[0])
+        if save_locally:
+            data_df.to_csv(local_path, index=False)
+            logging.info(f"Sheet saved locally to {local_path}")
+        return data_df
 
 
-def get_foodtech_search_terms(from_local=True, save_locally=True):
+def get_foodtech_search_terms(
+    from_local: bool = True, save_locally: bool = True
+) -> pd.DataFrame:
     """Get search terms for food tech project"""
-    local_path = PROJECT_DIR / "inputs/data/misc/foodtech/foodtech_search_terms.csv"
-    # Google sheets info
-    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    spreadsheet_id = "1O1PA6TvHHVyX1-lMAMoGYUhecQToGQpqv3l9GxXMbB8"
-    data_range = "search_terms"
-    if from_local:
-        logging.info(f"Loading search terms from {local_path}")
-        return pd.read_csv(local_path)
-    else:
-        data = pull_sheet_data(scopes, spreadsheet_id, data_range)
-        data_df = pd.DataFrame(data[1:], columns=data[0])
-        if save_locally:
-            data_df.to_csv(local_path, index=False)
-            logging.info(f"Search terms saved locally to {local_path}")
-        return data_df
+    return get_sheet_data(
+        local_path=PROJECT_DIR / "inputs/data/misc/foodtech/foodtech_search_terms.csv",
+        spreadsheet_id="1O1PA6TvHHVyX1-lMAMoGYUhecQToGQpqv3l9GxXMbB8",
+        data_range="search_terms",
+        from_local=from_local,
+        save_locally=save_locally,
+    )
 
 
-def get_foodtech_reviewed_vc(from_local=True, save_locally=True):
-    """Get search terms for food tech project"""
-    local_path = PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_VC.csv"
-    # Google sheets info
-    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    spreadsheet_id = "12D6cQXqMG9ou6XJbPpNw7S7tnr8nXK14r8XTD1BW5Fg"
-    data_range = "selected_companies_v2022_08_05"
-    if from_local:
-        logging.info(f"Loading search terms from {local_path}")
-        return pd.read_csv(local_path)
-    else:
-        data = pull_sheet_data(scopes, spreadsheet_id, data_range)
-        data_df = pd.DataFrame(data[1:], columns=data[0])
-        if save_locally:
-            data_df.to_csv(local_path, index=False)
-            logging.info(f"Search terms saved locally to {local_path}")
-        return data_df
+def get_foodtech_reviewed_vc(
+    from_local: bool = True, save_locally: bool = True
+) -> pd.DataFrame:
+    """Get reviewed VC data for food tech project"""
+    return get_sheet_data(
+        local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_VC.csv",
+        spreadsheet_id="12D6cQXqMG9ou6XJbPpNw7S7tnr8nXK14r8XTD1BW5Fg",
+        data_range="selected_companies_v2022_08_05",
+        from_local=from_local,
+        save_locally=save_locally,
+    )
 
 
-def get_foodtech_reviewed_gtr(from_local=True, save_locally=True):
-    """"""
-    local_path = PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_gtr.csv"
-    # Google sheets info
-    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    spreadsheet_id = "1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0"
-    data_range = "ukri"
-    if from_local:
-        logging.info(f"Loading search terms from {local_path}")
-        return pd.read_csv(local_path)
-    else:
-        data = pull_sheet_data(scopes, spreadsheet_id, data_range)
-        data_df = pd.DataFrame(data[1:], columns=data[0])
-        if save_locally:
-            data_df.to_csv(local_path, index=False)
-            logging.info(f"Search terms saved locally to {local_path}")
-        return data_df
+def get_foodtech_reviewed_gtr(
+    from_local: bool = True, save_locally: bool = True
+) -> pd.DataFrame:
+    """Get reviewed GtR data for food tech project"""
+    return get_sheet_data(
+        local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_gtr.csv",
+        spreadsheet_id="1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0",
+        data_range="ukri",
+        from_local=from_local,
+        save_locally=save_locally,
+    )
 
 
-def get_foodtech_reviewed_nihr(from_local=True, save_locally=True):
-    """"""
-    local_path = PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_gtr.csv"
-    # Google sheets info
-    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    spreadsheet_id = "1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0"
-    data_range = "nihr"
-    if from_local:
-        logging.info(f"Loading search terms from {local_path}")
-        return pd.read_csv(local_path)
-    else:
-        data = pull_sheet_data(scopes, spreadsheet_id, data_range)
-        data_df = pd.DataFrame(data[1:], columns=data[0])
-        if save_locally:
-            data_df.to_csv(local_path, index=False)
-            logging.info(f"Search terms saved locally to {local_path}")
-        return data_df
+def get_foodtech_reviewed_nihr(
+    from_local: bool = True, save_locally: bool = True
+) -> pd.DataFrame:
+    """Get reviewed NIHR data for food tech project"""
+    return get_sheet_data(
+        local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_nihr.csv",
+        spreadsheet_id="1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0",
+        data_range="nihr",
+        from_local=from_local,
+        save_locally=save_locally,
+    )
 
 
-def get_foodtech_heat_map(from_local=True, data_range="heatmap", save_locally=True):
-    """"""
-    local_path = PROJECT_DIR / "outputs/foodtech/interim/foodtech_heatmap.csv"
-    # Google sheets info
-    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    spreadsheet_id = "1SX_5jBSNtegyxVFo4CGvBoV0pPykZ0EKz8hui0kpLSo"
-    if from_local:
-        logging.info(f"Loading search terms from {local_path}")
-        return pd.read_csv(local_path)
-    else:
-        data = pull_sheet_data(scopes, spreadsheet_id, data_range)
-        data_df = pd.DataFrame(data[1:], columns=data[0])
-        if save_locally:
-            data_df.to_csv(local_path, index=False)
-            logging.info(f"Search terms saved locally to {local_path}")
-        return data_df
+def get_foodtech_heat_map(
+    from_local: bool = True, save_locally: bool = True
+) -> pd.DataFrame:
+    """Get heatmap data for food tech project"""
+    return get_sheet_data(
+        local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_heatmap.csv",
+        spreadsheet_id="1SX_5jBSNtegyxVFo4CGvBoV0pPykZ0EKz8hui0kpLSo",
+        data_range="heatmap",
+        from_local=from_local,
+        save_locally=save_locally,
+    )
 
 
 def get_foodtech_guardian(
-    from_local=True, data_range="guardian_final_hits", save_locally=True
-):
-    """"""
-    local_path = PROJECT_DIR / "outputs/foodtech/interim/guardian_final_hits.csv"
-    # Google sheets info
-    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-    spreadsheet_id = "1UuYDhLtkMt7RFN2WR9AcXTOS5rUmokhD7Cqdam1Nrsk"
-    if from_local:
-        logging.info(f"Loading search terms from {local_path}")
-        return pd.read_csv(local_path)
-    else:
-        data = pull_sheet_data(scopes, spreadsheet_id, data_range)
-        data_df = pd.DataFrame(data[1:], columns=data[0])
-        if save_locally:
-            data_df.to_csv(local_path, index=False)
-            logging.info(f"Search terms saved locally to {local_path}")
-        return data_df
+    from_local: bool = True, save_locally: bool = True
+) -> pd.DataFrame:
+    """Get guardian data for food tech project"""
+    return get_sheet_data(
+        local_path=PROJECT_DIR / "outputs/foodtech/interim/guardian_final_hits.csv",
+        spreadsheet_id="1UuYDhLtkMt7RFN2WR9AcXTOS5rUmokhD7Cqdam1Nrsk",
+        data_range="guardian_final_hits",
+        from_local=from_local,
+        save_locally=save_locally,
+    )
