9a663724f240a12abd1a55f61066c99940b53996
Jack Rasala <47244945+Jack-0-0@users.noreply.github.com>
2022-12-06 12:56:38 +0900

    [202] Add google sheets utils (#203)
    Closes #202.
    
    - Adds a new submodule `utils.google_sheets` to download and upload to Google Sheets.
    - Updates `getters.google_sheets` functions to use functions from `utils.google_sheets`

--- innovation_sweet_spots/getters/google_sheets.py
+++ innovation_sweet_spots/getters/google_sheets.py
@@ -6,81 +6,14 @@ Module for easy access to Google Sheets
 """
 from innovation_sweet_spots import PROJECT_DIR, logging
 import pandas as pd
-import pickle
-import os.path
-import dotenv
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
-from typing import Iterable
 from pathlib import Path
-
-
-def get_credentials_path() -> Path:
-    """Finds the path to the credentials file"""
-    if os.path.isfile(PROJECT_DIR / ".env"):
-        # Load the .env file
-        dotenv.load_dotenv(PROJECT_DIR / ".env")
-        try:
-            # Try fetching the path to credentials
-            return Path(os.environ["GOOGLE_SHEETS_CREDENTIALS"])
-        except:
-            pass
-    # If no .env file or if the key is not in the .env file
-    logging.warning("No credentials found!")
-    return None
-
-
-def gsheet_api_check(
-    scopes: Iterable[str] = ["https://www.googleapis.com/auth/spreadsheets"]
-):
-    """Authorise access to Google Sheets
-
-    Args:
-        scopes: ["https://www.googleapis.com/auth/spreadsheets"] or
-            ["https://www.googleapis.com/auth/spreadsheets"].
-
-    Returns:
-        Google credentials object
-    """
-    creds = None
-    credentials_path = get_credentials_path()
-    token_path = credentials_path.parent / "token.pickle"
-
-    if os.path.exists(token_path):
-        with open(token_path, "rb") as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, scopes)
-            creds = flow.run_local_server(port=0)
-        with open(token_path, "wb") as token:
-            pickle.dump(creds, token)
-    return creds
-
-
-def get_sheet_from_googlesheets(spreadsheet_id: str, data_range: str) -> list:
-    """Get data from Google Sheets"""
-    creds = gsheet_api_check()
-    service = build("sheets", "v4", credentials=creds)
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values().get(spreadsheetId=spreadsheet_id, range=data_range).execute()
-    )
-    values = result.get("values")
-    if values:
-        logging.info("Data copied from googlesheets")
-        return values
-    else:
-        logging.warning("No data found.")
+from innovation_sweet_spots.utils.google_sheets import download_google_sheet
 
 
 def get_sheet_data(
     local_path: Path,
-    spreadsheet_id: str,
-    data_range: str,
+    google_sheet_id: str,
+    wks_name: str,
     from_local=True,
     save_locally=True,
 ) -> pd.DataFrame:
@@ -88,37 +21,41 @@ def get_sheet_data(
 
     Args:
         local_path: Path to csv
-        spreadsheet_id: Googlesheets id, e.g '1O1PA6TvHHVyX1-lMAMoGYUhecQToGQpqv3l9GxXMbB8'
-        data_range: Sheet to load from googlesheet
+        google_sheet_id: Google Sheets id, e.g '1O1PA6TvHHVyX1-lMAMoGYUhecQToGQpqv3l9GxXMbB8'
+        wks_name: Worksheet to load from Google Sheet
         from_local: Set to True to load from local_path
         save_locally: Set to True to save data loaded from
-            googlesheets to local_path.
+            Google Sheets to local_path.
 
     Returns:
         Dataframe of loaded data
     """
     if from_local:
-        logging.info(f"Loading search terms from {local_path}")
+        logging.info(f"Loading data from {local_path}")
         return pd.read_csv(local_path)
     else:
-        data = get_sheet_from_googlesheets(spreadsheet_id, data_range)
-        data_df = pd.DataFrame(data[1:], columns=data[0])
+        data = download_google_sheet(google_sheet_id, wks_name)
         if save_locally:
-            data_df.to_csv(local_path, index=False)
+            data.to_csv(local_path, index=False)
             logging.info(f"Sheet saved locally to {local_path}")
-        return data_df
+        return data
 
 
 def get_foodtech_search_terms(
     from_local: bool = True, save_locally: bool = True
 ) -> pd.DataFrame:
     """Get search terms for food tech project"""
-    return get_sheet_data(
-        local_path=PROJECT_DIR / "inputs/data/misc/foodtech/foodtech_search_terms.csv",
-        spreadsheet_id="1O1PA6TvHHVyX1-lMAMoGYUhecQToGQpqv3l9GxXMbB8",
-        data_range="search_terms",
-        from_local=from_local,
-        save_locally=save_locally,
+    return (
+        get_sheet_data(
+            local_path=PROJECT_DIR
+            / "inputs/data/misc/foodtech/foodtech_search_terms.csv",
+            google_sheet_id="1O1PA6TvHHVyX1-lMAMoGYUhecQToGQpqv3l9GxXMbB8",
+            wks_name="search_terms",
+            from_local=from_local,
+            save_locally=save_locally,
+        )
+        .reset_index()
+        .rename(columns={"index": "Category"})
     )
 
 
@@ -128,8 +65,8 @@ def get_foodtech_reviewed_vc(
     """Get reviewed VC data for food tech project"""
     return get_sheet_data(
         local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_VC.csv",
-        spreadsheet_id="12D6cQXqMG9ou6XJbPpNw7S7tnr8nXK14r8XTD1BW5Fg",
-        data_range="selected_companies_v2022_08_05",
+        google_sheet_id="12D6cQXqMG9ou6XJbPpNw7S7tnr8nXK14r8XTD1BW5Fg",
+        wks_name="selected_companies_v2022_08_05",
         from_local=from_local,
         save_locally=save_locally,
     )
@@ -139,12 +76,17 @@ def get_foodtech_reviewed_gtr(
     from_local: bool = True, save_locally: bool = True
 ) -> pd.DataFrame:
     """Get reviewed GtR data for food tech project"""
-    return get_sheet_data(
-        local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_gtr.csv",
-        spreadsheet_id="1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0",
-        data_range="ukri",
-        from_local=from_local,
-        save_locally=save_locally,
+    return (
+        get_sheet_data(
+            local_path=PROJECT_DIR
+            / "outputs/foodtech/interim/foodtech_reviewed_gtr.csv",
+            google_sheet_id="1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0",
+            wks_name="ukri",
+            from_local=from_local,
+            save_locally=save_locally,
+        )
+        .reset_index()
+        .rename(columns={"index": "id"})
     )
 
 
@@ -152,12 +94,17 @@ def get_foodtech_reviewed_nihr(
     from_local: bool = True, save_locally: bool = True
 ) -> pd.DataFrame:
     """Get reviewed NIHR data for food tech project"""
-    return get_sheet_data(
-        local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_reviewed_nihr.csv",
-        spreadsheet_id="1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0",
-        data_range="nihr",
-        from_local=from_local,
-        save_locally=save_locally,
+    return (
+        get_sheet_data(
+            local_path=PROJECT_DIR
+            / "outputs/foodtech/interim/foodtech_reviewed_nihr.csv",
+            google_sheet_id="1ZZQO6m6BSIiwTqgfHq9bNaf_FB1HG4EqwedgDLzESa0",
+            wks_name="nihr",
+            from_local=from_local,
+            save_locally=save_locally,
+        )
+        .reset_index()
+        .rename(columns={"index": "id"})
     )
 
 
@@ -165,12 +112,16 @@ def get_foodtech_heat_map(
     from_local: bool = True, save_locally: bool = True
 ) -> pd.DataFrame:
     """Get heatmap data for food tech project"""
-    return get_sheet_data(
-        local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_heatmap.csv",
-        spreadsheet_id="1SX_5jBSNtegyxVFo4CGvBoV0pPykZ0EKz8hui0kpLSo",
-        data_range="heatmap",
-        from_local=from_local,
-        save_locally=save_locally,
+    return (
+        get_sheet_data(
+            local_path=PROJECT_DIR / "outputs/foodtech/interim/foodtech_heatmap.csv",
+            google_sheet_id="1SX_5jBSNtegyxVFo4CGvBoV0pPykZ0EKz8hui0kpLSo",
+            wks_name="heatmap",
+            from_local=from_local,
+            save_locally=save_locally,
+        )
+        .reset_index()
+        .rename(columns={"index": "Category"})
     )
 
 
@@ -178,10 +129,14 @@ def get_foodtech_guardian(
     from_local: bool = True, save_locally: bool = True
 ) -> pd.DataFrame:
     """Get guardian data for food tech project"""
-    return get_sheet_data(
-        local_path=PROJECT_DIR / "outputs/foodtech/interim/guardian_final_hits.csv",
-        spreadsheet_id="1UuYDhLtkMt7RFN2WR9AcXTOS5rUmokhD7Cqdam1Nrsk",
-        data_range="guardian_final_hits",
-        from_local=from_local,
-        save_locally=save_locally,
+    return (
+        get_sheet_data(
+            local_path=PROJECT_DIR / "outputs/foodtech/interim/guardian_final_hits.csv",
+            google_sheet_id="1UuYDhLtkMt7RFN2WR9AcXTOS5rUmokhD7Cqdam1Nrsk",
+            wks_name="guardian_final_hits",
+            from_local=from_local,
+            save_locally=save_locally,
+        )
+        .reset_index()
+        .rename(columns={"index": "id"})
     )
